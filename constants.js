d741fb9d9fa2ead22f2e136ec2bec4ee7135cfb7
Suryansh Pandey <suryanshpsurya@gmail.com>
2023-10-11 09:44:10 +0530

    changes in url

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = `http://52.55.72.246:8080/api/broker/`
-export const url = `http://52.55.72.246:8080`
+export const apiUrl = `http://52.55.72.246:8000/api/broker/`
+export const url = `http://52.55.72.246:8000`
