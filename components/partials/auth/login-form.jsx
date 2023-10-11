25378295e9135dcdf2b6d41747b5f3ac56d5b23d
Suryansh Pandey <suryanshpsurya@gmail.com>
2023-10-11 09:39:13 +0530

    changes

--- components/partials/auth/login-form.jsx
+++ components/partials/auth/login-form.jsx
@@ -9,7 +9,6 @@ import { handleLogin } from "./store";
 import { toast } from "react-toastify";
 import Button from "@/components/ui/Button";
 import { Icon } from "@iconify/react";
-import { url } from "@/constants";
 const schema = yup
   .object({
     email: yup.string(),
